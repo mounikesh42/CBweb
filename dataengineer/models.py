d79c3ce8430c83082f69b542ab1ccba117449d2c
unknown <thotamounikesh@gmail.com>
2024-09-20 12:01:16 +0530

    added iamge validation

--- dataengineer/models.py
+++ dataengineer/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from django.db import models
 
 class ImageModel(models.Model):
-    name = models.CharField(max_length=255)  
+    name = models.CharField(max_length=255,unique=True)  
     image = models.ImageField(upload_to='images/') 
     created_at = models.DateTimeField(auto_now_add=True)
     def __str__(self):
